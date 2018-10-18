37a8571c8bf2885a277522f62af375cd9ad9ffc7
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2018-10-18 21:29:37 +0100

    .

--- site.pp
+++ site.pp
@@ -1,14 +1,21 @@
 node default {
+
+  ensure_packages(['MySQL-python','pyOpenSSL','python-ldap','python-memcached','python-psycopg2','python-zope-interface'])
+
   selinux::boolean{ 'httpd_can_network_connect':
     ensure => 'on',
   }
+
   selinux::boolean{ 'httpd_setrlimit':
     ensure => 'on',
   }
+
   selinux::port{ 'allow-http-redirect-port':
       seltype  => 'http_port_t',
       protocol => 'tcp',
       port     => lookup('graphite::gr_web_server_port'),
   }
+
   include ::graphite
+
 }
